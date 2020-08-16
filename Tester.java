4d6778f598c8f5244580f6f6ad969b1a4b5c3670
saadkoker <60635704+saadkoker@users.noreply.github.com>
2020-08-16 00:38:53 -0400

    Added the board files

---
+++ Tester.java
@@ -0,0 +1,35 @@
+import java.awt.color.*;
+import javax.swing.*;
+import java.awt.Color;
+
+public class Tester{
+	
+	public static void main(String[] args){
+		
+		Board board = new Board(10, 10, "Mustafa Nigga");
+
+		try {
+			Thread.sleep(1100);
+		} catch (Exception e) {
+			//TODO: handle exception
+		}
+
+		while (true) {
+			
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {
+				//TODO: handle exception
+			}
+			board.vibrate(10, 50000);
+			board.changeBoardColor("red", "blue");
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {
+				//TODO: handle exception
+			}
+			board.vibrate(10, 50000);
+			board.changeBoardColor(new Color(0,0,0), new Color(255,255,255));
+		}
+	}
+}
