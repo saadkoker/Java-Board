28b44a27d7c8e3156dbedfbfbee8c3563c71ce39
saadkoker <60635704+saadkoker@users.noreply.github.com>
2020-08-16 01:12:29 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -403,3 +403,25 @@ public class Board extends JPanel
 		}
 	}
 }
+
+public class Coordinate
+{
+  private int row;
+  private int col;
+  
+  public Coordinate(int theRow, int theCol)
+  {
+    row = theRow;
+    col = theCol;
+  }
+  
+  public int getRow()
+  {
+    return row;
+  }
+    
+  public int getCol()
+  {
+    return col;
+  }
+} 
